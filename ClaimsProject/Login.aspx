b3071a004e1494db7726e1e40a34d0b8d72b6b77
gituser <aavila@costex.com>
2021-07-30 15:37:57 -0400

    add login form

---
+++ ClaimsProject/Login.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" MasterPageFile="~/Site.Master" CodeBehind="Login.aspx.vb" Inherits="ClaimsProject.Login" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="container" runat="server">
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <div id="pdDisplayLogin" class="shadow-to-box">
+                    <!-- first row -->
+                    <div class="row" style="padding: 30px 0;">
+                        <div class="col-md-2"></div>
+                        <div class="col-md-8 centered"><span id="spnLoadText">Welcome to the CTP System</span></div>
+                        <div class="col-md-2"></div>
+                    </div>
+                    <!-- Second row -->
+                    <div class="form-row">
+                        <div class="col-md-1"></div>
+                        <div class="col-md-5">
+                            <%--<asp:Label ID="lblUser" Text="User" CssClass="control-label" runat="server"> </asp:Label>--%>
+                            <div class="input-group-append">
+                                <asp:TextBox ID="txtUser" CssClass="form-control" name="txt-user" placeholder="USER" runat="server"></asp:TextBox>
+                                <span class="input-group-addon"><i class="fa fa-hashtag center-vert font-awesome-custom"></i></span>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <%--<asp:Label ID="lblPass" Text="Password" CssClass="control-label" runat="server"></asp:Label>--%>
+                            <div class="input-group-append">
+                                <asp:TextBox ID="txtPass" CssClass="form-control" name="txt-pass" placeholder="PASSWORD" runat="server"></asp:TextBox>
+                                <span class="input-group-addon"><i class="fa fa-hashtag center-vert font-awesome-custom"></i></span>
+                            </div>
+                        </div>
+                        <div class="col-md-1"></div>
+                    </div>
+                    <!-- Third row -->
+                    <div class="row" style="padding: 20px 0;">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-4"> 
+                            <asp:Button ID="btnLoginUser" Text="   Login   " class="btn btn-primary btn-lg btnFullSize" OnClick="btnLogin_Click" runat="server" />
+                        </div>
+                        <div class="col-md-4"></div>
+                    </div>
+                </div>                
+            </div>
+            <div class="col-md-3"></div>
+        </div>        
+    </div>
+</asp:Content>
---
+++ ClaimsProject/Login.aspx.vb
@@ -0,0 +1,82 @@
+﻿Imports System.DirectoryServices
+
+Public Class Login
+    Inherits UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Structure messageType
+        Const success = "success"
+        Const warning = "warning"
+        Const info = "info"
+        Const [Error] = "Error"
+    End Structure
+
+    Protected Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLoginUser.Click
+        Dim methodMessage As String = Nothing
+        Try
+            Dim user = txtUser.Text.Trim()
+            Dim pass = txtPass.Text.Trim()
+
+            If AuthenticateUser(user, pass) Then
+                Response.Redirect("Default.aspx")
+            Else
+                methodMessage = "There is an error in the credential validation for the user: " + user.ToUpper()
+                SendMessage(methodMessage, messageType.info)
+            End If
+            'SearchOneUser(user)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Function getLDAPConnectionString(defSearch As String, ByRef strLdap As String, Optional flag As Boolean = False) As Boolean
+        Dim blResult As Boolean = False
+        Try
+            Dim de As DirectoryEntry = New DirectoryEntry("LDAP://RootDSE")
+            If de IsNot Nothing Then
+                If flag Then
+                    strLdap = "LDAP://" + de.Properties("defaultNamingContext")(0).ToString()
+                Else
+                    strLdap = de.Properties("defaultNamingContext")(0).ToString()
+                End If
+
+                'strLdap = If(flag, strLdap = "LDAP://" + de.Properties("defaultNamingContext")(0).ToString(), strLdap = de.Properties("defaultNamingContext")(0).ToString())
+                blResult = If(String.IsNullOrEmpty(de.Properties("defaultNamingContext")(0).ToString()), False, True)
+            End If
+
+            Return blResult
+        Catch ex As Exception
+            Dim msg = ex.Message
+            Return blResult
+        End Try
+    End Function
+
+    Public Function AuthenticateUser(userName As String, password As String) As Boolean
+        Dim blResult As Boolean = False
+        Dim strLdap As String = Nothing
+        Try
+            Dim blFlag = getLDAPConnectionString("", strLdap, True)
+            If blFlag Then
+                Dim de As DirectoryEntry = New DirectoryEntry(strLdap, userName, password)
+                Dim dSearch As DirectorySearcher = New DirectorySearcher(de)
+                Dim results As SearchResult = Nothing
+
+                results = dSearch.FindOne()
+                blResult = True
+            End If
+
+            Return blResult
+        Catch ex As Exception
+            Dim msg = ex.Message
+            Return blResult
+        End Try
+    End Function
+
+    Public Sub SendMessage(methodMessage As String, detailInfo As String)
+        ScriptManager.RegisterStartupScript(Me, Page.GetType, "Message", "messageFormSubmitted('" & methodMessage & " ', '" & detailInfo & "')", True)
+    End Sub
+
+End Class
